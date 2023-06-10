dadd72228d0e1a6a275fdac033e9c860110166ad
Flavio Santos <flavio.pasantos@outlook.com>
2023-06-10 01:01:42 -0300

    stc: create component from cardItem

--- code/src/App.vue
+++ code/src/App.vue
@@ -1,22 +1,38 @@
 <template>
   <div id="app">
     <BestTransport/>
-    <FormTransportPage/>
+    <div class="container-body-page">
+      <FormTransportPage/>
+      <ItemCard message="Frete com menor valor" tranpostName="Sedex" leadTime="2" price="2.5"/>
+    </div>
+   
   </div>
 </template>
 
 <script>
 import BestTransport from './components/BestTransport.vue'
 import FormTransportPage from './components/FormTransportPage.vue';
+import ItemCard from './components/ItemCard.vue';
 
 export default {
   name: 'App',
   components: {
     BestTransport,
-    FormTransportPage
+    FormTransportPage,
+    ItemCard
   }
 }
 </script>
 
 <style>
+    .container-body-page{
+      display: flex;
+      align-items: center;
+      justify-content: space-evenly;
+      background-color: #F8F9FA;
+      margin: 30px;
+      border-radius: 8px;
+      box-shadow: -5px 2px 18px -6px rgba(0,0,0,0.75);
+;
+    }
 </style>
---
+++ code/src/components/ItemCard.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="container-card">
+        <div class="top-message">
+            <p>Estas são as melhores alternativas de frete que encontramos para você</p>
+        </div>
+        <div class="container-informations">
+            <section class="data-transport">
+                <h4>{{message}}</h4>
+                <div class="figure">
+                    <span>vai entrar uma imagem</span>
+                </div>
+               <p>Transportadora: <span>{{tranpostName}}</span></p>
+               <p>Tempo Estimando: <span>{{leadTime}}</span></p>
+            </section>
+            <section class="data-value">
+                <div class="figure">
+                    <span>vai entrar uma imagem</span>
+                </div>
+                <p> Preço: <span>{{price}}</span></p>
+            </section>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ItemCard",
+
+        props: {
+            message: String,
+            tranpostName: String,
+            leadTime: String,
+            price: String
+        }, 
+    }
+</script>
+
+<style>
+    .container-informations{
+        width: 100%;
+        padding: 5px;
+        display: flex;
+        justify-content: space-between;
+    }
+    .data-transport{
+        padding: 10px;
+        border-radius: 4px;
+        display: flex;
+        flex-direction: column;
+        align-content: center;
+        justify-content: center;
+        flex: 1;
+        background-color: #CCCCCC ;
+    }
+    .data-value{
+        display: flex;
+        border-radius: 4px;
+        align-items: center;
+        justify-content: center;
+        background-color: #CCCCCC ;
+        padding: 10px;
+        width: 30%;
+        margin-left: 10px;
+    }
+    .top-message{
+        text-align: left;
+        width: 80%;
+        color: #6C757D;
+        font-size: medium;
+        font-weight: lighter;
+    }
+</style>
